bbc686114aa1154cc42016e27c44cc891ff3f1f5
Haddouche Maamar <frocodeof@gmail.com>
2023-06-12 00:42:53 +0200

    First

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+    console.log("helo")
+    console.log("helo")
+    const title = "Welcome to the new Blog"
+    let likes = 50;
+    const person = {name: "yoshi", age: 30}
+    const link = "http://www.google.com"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="contnent">
+            <h1>{title} </h1>
+            <p>{likes} </p>
+            <p>{ 10 }</p>
+            <p>{ "hello ninjas " }</p>
+            <p>{ Math.random()  * 0.5 * 10 * Math.floor() }</p>
+            <a href={link}>Google</a>
+      </div>
     </div>
   );
 }
