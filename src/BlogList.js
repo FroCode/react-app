bde63db4d53fb2ce590ca4a76bbab0b3b1c513f5
Haddouche Maamar <frocodeof@gmail.com>
2023-06-15 17:06:53 +0200

    Add function delete button

---
+++ src/BlogList.js
@@ -0,0 +1,16 @@
+
+
+const BlogList = (blogs ,  title , handleDelete) => {
+    return (
+        <div className="blog-list">
+            {blogs.map((blog) => (
+                <div className="blogPreview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written By : {blog.author}</p>
+                    <button onClick={()=> handleDelete(blog.id)} >Delete Blog </button>
+                </div>
+            ))}
+        </div>
+    )
+}
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
 
-
+import BlogList from './BlogList';
 
 const Home = () => {
+    // Start My Component
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blogPreview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>{blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} handleDelete={handleDelete} />
         </div>
     );
 }
+
+
 export default Home;
