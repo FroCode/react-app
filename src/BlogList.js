bda2cecb13a947588a155e37153e064f6e74dc05
Haddouche Maamar <frocodeof@gmail.com>
2023-06-15 23:28:43 +0200

    BlogList.js updated

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,25 +1,17 @@
+const BlogList = ( {blogs , title , handleDelete}) => {
+   
 
+  return (
+    <div className="blog-list">
+      {blogs.map((blog) => (
+        <div className="blogPreview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written By: {blog.author}</p>
 
-// const BlogList = (props) => {
-const BlogList = (blogs ,  title , handleDelete) => {
-
-const BlogList = (props) => {
-    const blogs = props.blogs;
-    console.log(props, blogs)
-}
-    return (
-        <div className="blog-list">
-            {blogs.map((blog) => (
-                <div className="blogPreview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written By : {blog.author}</p>
-
-                    <button onClick={()=> handleDelete(blog.id)} >Delete Blog </button>
-
-
-                </div>
-            ))}
+          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 export default BlogList;
