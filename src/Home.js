1cb3769f3be38d887e2359540b7c6b6eea52c383
Haddouche Maamar <frocodeof@gmail.com>
2023-06-16 11:57:02 +0200

    Nothinh to commit, working tree clean ```

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect ,useState } from 'react';
+
 
 import BlogList from './BlogList';
 
@@ -9,14 +10,20 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
+        
     }
+    useEffect(() => {
+        console.log('use effect ran');
+    }
+    );  
     return (
         <div className="home">
             <BlogList blogs={blogs} handleDelete={handleDelete} />
-            <BlogList blogs={blogs} />
+            
         </div>
     );
 }
