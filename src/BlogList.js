2d7b93b2cd22d0db3cb98cdef74653ed32c4b5e3
Haddouche Maamar <frocodeof@gmail.com>
2023-06-17 22:19:46 +0200

    Without Data

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,17 +1,23 @@
-const BlogList = ( {blogs , title , handleDelete}) => {
-   
+const BlogList = ( {blogs, handleDelete}) => {
+
+
 
   return (
+
+
     <div className="blog-list">
       {blogs.map((blog) => (
         <div className="blogPreview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written By: {blog.author}</p>
-
           <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
         </div>
       ))}
     </div>
   );
+
+
+
+
 }
 export default BlogList;
