6a31660d08a47535716514a049f6aed784eca405
Haddouche Maamar <frocodeof@gmail.com>
2023-06-18 21:30:01 +0200

    Exact Match Routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
-
+    <Router>
     <div className="App">
         <Navbar />
         <div className="content">
 
-            <Home />
+            <Switch>
+              <Route exact path="/">
+                <Home />
+
+              </Route>
+            </Switch>
+            
         </div>
     </div>
+    </Router>
   );
 }
 
