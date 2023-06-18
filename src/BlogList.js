0ff274af87b441a1a3fa7bb2e3524c6633092526
Haddouche Maamar <frocodeof@gmail.com>
2023-06-18 17:12:11 +0200

    Fteching Data

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,14 +1,17 @@
-const BlogList = ( {blogs, handleDelete}) => {
+const BlogList = ( {blogs , title, handleDelete}) => {
 
 
 
   return (
-
+    
     <div className="blog-list">
+      <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blogPreview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written By: {blog.author}</p>
+          <span>{blog.date}</span>
+          <span>{blog.author}</span>
           <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
         </div>
       ))}
--- src/Home.js
+++ src/Home.js
@@ -11,21 +11,40 @@ const Home = () => {
         setBlogs(newBlogs);
         
     }
+    const [error, setError] = useState(null); // [state, setState
+    const [pending, setPending] = useState(true);
+    
     useEffect(() => { 
-        fetch('http://localhost:8000/blogs/').then(res => {
-            return res.json()
-        })
-        .then(data => {
-            setBlogs(data);
-            console.log(data);
-        });
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs/').then(res => {
+                console.log(res);
+                if(!res.ok){
+                    throw Error('Could not fetch the data for that resource');
+                } 
+                return res.json()
+            })
+            .then(data => {
+                setBlogs(data);
+                setPending(false);
+            }).catch(err => {
+                console.log(err.message);
+                
+
+            });
+            
+        },);
+       
     },[]);  
     return (
+        
         <div className="home">
-            {blogs && <BlogList blogs={blogs} handleDelete={handleDelete} />}
+            {pending && <div className='loading'>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} handleDelete={handleDelete} title="All Blogs !" />}
             
         </div>
+         
     );
+    
 }
 
 
